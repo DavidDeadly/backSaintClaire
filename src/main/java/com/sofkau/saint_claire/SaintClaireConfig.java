f4262aaf3d3e54aaf85552b9347671e1dd1343d8
DavidDeadly <jdrueda513@gmail.com>
2022-07-28 15:49:57 -0500

    Cors config

--- src/main/java/com/sofkau/saint_claire/SaintClaireConfig.java
+++ src/main/java/com/sofkau/saint_claire/SaintClaireConfig.java
@@ -7,13 +7,27 @@ import com.sofkau.saint_claire.services.specialty.SpecialtyRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class SaintClaireConfig {
+public class SaintClaireConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+      .addMapping("/**")
+      .allowedHeaders("*")
+      .allowedMethods("*")
+      .allowedOrigins("http://localhost:3000")
+      .allowCredentials(false)
+      .maxAge(-1);
+    WebMvcConfigurer.super.addCorsMappings(registry);
+  }
 
   @Bean
   CommandLineRunner commandLineRunner(
